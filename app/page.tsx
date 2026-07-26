aa44cd4b7311439f17010417e8851f72bf501569
Berkant Soytas <mailtoberkant@gmail.com>
2026-07-27 01:49:09 +0300

    chore: add vercel analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import TerminalHero from "@/components/TerminalHero";
 import RecentSection from "@/components/RecentSection";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col gap-16">
-      <TerminalHero />
-      <RecentSection />
-    </div>
+    <>
+      <div className="flex flex-col gap-16">
+        <TerminalHero />
+        <RecentSection />
+      </div>
+      <Analytics />
+    </>
   );
 }
